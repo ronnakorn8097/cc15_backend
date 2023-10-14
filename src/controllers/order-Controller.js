f25b558304e573c3711aaf0d0039d56b6bb984f2
ronnakorn8097 <ronnakorn.fahkiat@outlook.com>
2023-10-14 11:59:35 +0700

    order by korn

--- src/controllers/order-Controller.js
+++ src/controllers/order-Controller.js
@@ -1,18 +1,75 @@
+const prisma = require("../models/prisma");
+const createError = require("../utils/create-error");
 
-const prisma = require('../models/prisma');
-const createError = require('../utils/create-error');
-
-
-exports.createOrder = async (req,res,next) => {
-    try {
-        const order = req.body
-        await prisma.orderMenu.create({
-       
-        })
-    } catch (error) {
-        next(error)
+exports.createOrder = async (req, res, next) => {
+  try {
+    const { paymentType, orderType, discount, orderMenus } = req.body;
+    
+    let totalPrice = "0.00"
+    const orderMenusForCreate = [];
+    const menuIds = [];
+    for (let orderMenu of orderMenus) {
+      menuIds.push(orderMenu.menuId);
+      orderMenusForCreate.push({
+        amounts: orderMenu.amounts,
+        menus: {
+          connect: {
+            id: orderMenu.menuId,
+          },
+        },
+      });
     }
-}
 
+    const user = await prisma.users.findUnique({
+      where: {
+        id: req.user.id,
+      },
+    });
+    const menus = await prisma.menus.findMany({
+      where: {
+        id: {
+          in: menuIds,
+        },
+      },
+    });
+    const notFoundMenus = menus.filter((e) => !menuIds.includes(e.id));
+    if (notFoundMenus.length > 0) {
+      return next(createError("Some menu ID not found", 404));
+    }
+    if (!user) {
+      return next(createError("User or Menu not found", 404));
+    }
 
+    if (notFoundMenus.length === 0 && user) {
+        for(let menu of menus) {
+            let amounts = orderMenus.filter(e => e.menuId === menu.id)[0].amounts
+            console.log(`Amounts: ${amounts}`)
+            let floatTotalPrice = parseFloat(totalPrice) + parseFloat(menu.price)*parseFloat(amounts)
+            totalPrice = floatTotalPrice.toFixed(2)
+        }
+        console.log(`TotalPrice: ${totalPrice}`)
+        if (discount !== "0.00") {
+            let floatTotalPrice = parseFloat(totalPrice)+parseFloat(discount)
+            totalPrice = floatTotalPrice.toFixed(2)
+        }
+        console.log(`After Discount TotalPrice: ${totalPrice}`)
+      const newOrder = await prisma.orders.create({
+        data: {
+          userId: req.user.id, // assuming a user with ID 1 exists
+          paymentType,
+          orderType,
+          totalPrice, // should ideally be of type Float or Int
+          discount,
+          orderMenus: {
+            create: orderMenusForCreate,
+          },
+        },
+      });
 
+      res.status(201).json({ message: "Create order success", order: newOrder });
+    }
+
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routes/orderRoute.js
+++ src/routes/orderRoute.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 
 
-router.post('/create',authenToken,orderController.createOrder)
+router.post('/',authenToken,orderController.createOrder)
 
 
 module.exports = router
